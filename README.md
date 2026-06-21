5cbc6f799ebfb2eb9aa8d3b49ca24fb4a3f32fa6
Alex Fedotov <alxfed@gmail.com>
2026-06-21 08:49:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Package name
+<pre>
+  pip install package-name
+</pre>
+Then:
+```Python
+  # Python
+  import package_name
+```
---
+++ src/package_name/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+
+def main(): # arg=None):
+    print(' main.')
+
+
+if __name__ == '__main__':
+    main()
